f98f6accbb5a00621048af1775ee3b6498274538
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2025-01-02 18:19:34 +0100

    lesson7-hw

--- backend/core/services/jwt_service.py
+++ backend/core/services/jwt_service.py
@@ -17,6 +17,10 @@ class ActivateToken(ActionToken):
     token_type = ActionTokenEnum.ACTIVATE.token_type
     lifetime = ActionTokenEnum.ACTIVATE.lifetime
 
+class RecoveryToken(ActionToken):
+    token_type = ActionTokenEnum.RECOVERY.token_type
+    lifetime = ActionTokenEnum.RECOVERY.lifetime
+
 
 class JWTService:
     @staticmethod
