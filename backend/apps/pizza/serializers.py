91005aee839d48953ecb9ee0498fdb1ee0bdd21d
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2025-01-10 20:55:21 +0100

    lesson10

--- backend/apps/pizza/serializers.py
+++ backend/apps/pizza/serializers.py
@@ -8,19 +8,22 @@ class PizzaSerializer(serializers.ModelSerializer):
         model = PizzaModel
         fields = ('id', 'name', 'size', 'price', 'day', 'updated_at', 'created_at')
 
-    def validate_price(self, price):
-        if price <= 0:
-            raise serializers.ValidationError('Price must be greater than 0')
-        return price
+    def create(self, validated_data):
+        return PizzaModel.objects.create(**validated_data, pizza_shop_id=1)
 
-    def validate(self, attrs):
-        price = attrs.get('price')
-        size = attrs.get('size')
-
-        if price == size:
-            raise serializers.ValidationError('Price cannot be equal to size')
-
-        return attrs
+    # def validate_price(self, price):
+    #     if price <= 0:
+    #         raise serializers.ValidationError('Price must be greater than 0')
+    #     return price
+    #
+    # def validate(self, attrs):
+    #     price = attrs.get('price')
+    #     size = attrs.get('size')
+    #
+    #     if price == size:
+    #         raise serializers.ValidationError('Price cannot be equal to size')
+    #
+    #     return attrs
 
 
 class PizzaPhotoSerializer(serializers.ModelSerializer):
