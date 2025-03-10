9d1d9f697d3697186ae0642bc2ac28b8356d44c7
Vitalii Demchyshyn <demchyshyn.v87@gmail.com>
2025-03-10 07:12:55 +0100

    lesson2-hw

--- configs/settings.py
+++ configs/settings.py
@@ -70,10 +70,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "python_drf",
-        "USER": "root",
-        "PASSWORD": "root",
+        "USER": "user",
+        "PASSWORD": "user",
         "HOST": "127.0.0.1",
-        "PORT": "3306",
+        "PORT": "3307",
     }
 }
 
